b09557c3ba03ed2f8dc50ffcf0d654d872021bb2
Vivek Poddar <vivekimsit@gmail.com>
2015-01-29 23:34:43 +0530

    Get the basic functionality.

--- build/app.js
+++ build/app.js
@@ -1,4 +1,126 @@
-React.render(
-  React.createElement("div", null, "Hello World"),
-  document.getElementById('example')
-);
+'use strict';
+
+var app = app || {};
+
+
+/* Models */
+app.TodoModel = function(opts) {
+  this.text = !!opts ? opts.text : '';
+  this.done = !!opts ? opts.done : false;
+};
+
+
+app.TodoList = Array;
+
+
+/* View model */
+app.TodoVM = (function(){
+  var vm = {};
+  vm.init = function() {
+    vm.list = new app.TodoList();
+
+    vm.newEntry = '';
+
+    vm.add = function() {
+      if (vm.newEntry) {
+        vm.list.push(new app.Todo({text: vm.newEntry}));
+        vm.newEntry = '';
+      }
+    };
+
+    vm.remove = function() {
+      vm.list = vm.list.filter(function(task) {
+        return !task.done;
+      });
+    }
+  }
+  return vm;
+}());
+
+
+/* Views */
+var TodoList = React.createClass({displayName: "TodoList",
+  render: function() {
+    var createItem = function(text) {
+      return React.createElement("li", null, text);
+    };
+    return React.createElement("ul", null, this.props.items.map(createItem));
+  }
+});
+
+
+var TodoApp = React.createClass({displayName: "TodoApp",
+  getInitialState: function() {
+    console.log(this.props);
+    var model = this.props.model;
+    model.init();
+    return {items: model.list, newEntry: model.newEntry};
+  },
+
+  handleKeyDown: function(e) {
+    if (e.which === 1) {
+      return;
+    }
+
+    e.preventDefault();
+    this.setState({
+      items: this.state.items.add(),
+      newEntry: ''
+    });
+  },
+
+  onChange: function() {
+    console.log('In onchange');
+  },
+
+  render: function() {
+    var main;
+    var todos = this.state.items;
+
+    var header = (
+      React.createElement("div", null, 
+        React.createElement("header", null, 
+          React.createElement("h1", null, "TODO")
+        )
+      )
+    );
+
+    if (todos.length) {
+      main = (
+        React.createElement("section", {id: "main"}, 
+          React.createElement(TodoList, {items: todos})
+        )
+      );
+    }
+
+    var footer = (
+      React.createElement("form", {onSubmit: this.handleSubmit, id: "form-submit"}, 
+        React.createElement("input", {id: "newValue", 
+               onChange: this.onChange, 
+               value: this.state.newEntry, 
+               placeholder: "I need do..."}
+        ), 
+        React.createElement("button", null, "Add")
+      )
+    );
+
+    return (
+      React.createElement("div", null, 
+        header, 
+        main, 
+        footer
+      )
+    );
+  }
+});
+
+
+function render() {
+  React.render(
+    React.createElement(TodoApp, {model: app.TodoVM}),
+    document.getElementById('todoapp')
+  );
+};
+
+
+render();
--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 'use strict';
+
 var app = app || {};
 
 
 /* Models */
 app.TodoModel = function(opts) {
-  this.description = !!opts ? opts.description : '';
+  this.text = !!opts ? opts.text : '';
   this.done = !!opts ? opts.done : false;
 };
 
@@ -16,14 +17,14 @@ app.TodoList = Array;
 app.TodoVM = (function(){
   var vm = {};
   vm.init = function() {
-    vm.list = new todo.TodoList();
+    vm.list = new app.TodoList();
 
-    vm.description = '';
+    vm.newEntry = '';
 
     vm.add = function() {
-      if (vm.description) {
-        vm.list.push(new app.Todo({description: vm.description}));
-        vm.description = '';
+      if (vm.newEntry) {
+        vm.list.push(new app.Todo({text: vm.newEntry}));
+        vm.newEntry = '';
       }
     };
 
@@ -38,26 +39,86 @@ app.TodoVM = (function(){
 
 
 /* Views */
-var TodoList = React.createClass({});
-
-
-var TodoApp = React.creatClass({
-  getInitialState: function() {
-    var vm = vm.init();
-    return {items: vm.list, newItem: vm.description};
-  },
-  handleSubmit: function() {},
+var TodoList = React.createClass({
   render: function() {
-    // TODO
-    return ;
+    var createItem = function(text) {
+      return <li>{text}</li>;
+    };
+    return <ul>{this.props.items.map(createItem)}</ul>;
+  }
+});
+
+
+var TodoApp = React.createClass({
+  getInitialState: function() {
+    console.log(this.props);
+    var model = this.props.model;
+    model.init();
+    return {items: model.list, newEntry: model.newEntry};
+  },
+
+  handleKeyDown: function(e) {
+    if (e.which === 1) {
+      return;
+    }
+
+    e.preventDefault();
+    this.setState({
+      items: this.state.items.add(),
+      newEntry: ''
+    });
+  },
+
+  onChange: function() {
+    console.log('In onchange');
+  },
+
+  render: function() {
+    var main;
+    var todos = this.state.items;
+
+    var header = (
+      <div>
+        <header>
+          <h1>TODO</h1>
+        </header>
+      </div>
+    );
+
+    if (todos.length) {
+      main = (
+        <section id="main">
+          <TodoList items={todos}/>
+        </section>
+      );
+    }
+
+    var footer = (
+      <form onSubmit={this.handleSubmit} id="form-submit">
+        <input id="newValue"
+               onChange={this.onChange}
+               value={this.state.newEntry}
+               placeholder="I need do..."
+        />
+        <button>Add</button>
+      </form>
+    );
+
+    return (
+      <div>
+        {header}
+        {main}
+        {footer}
+      </div>
+    );
   }
 });
 
 
 function render() {
   React.render(
-    <TodoApp model={model}/>,
-    document.getElementById('todoapp');
+    <TodoApp model={app.TodoVM} />,
+    document.getElementById('todoapp')
   );
 };
 
